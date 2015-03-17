782be374f508b5961157586a64d1e28205f1e1b8
Hill Wyrough <alexander.hill.wyrough@gmail.com>
2015-03-16 21:02:05 -0400

    Builds BigQuery Uploader

---
+++ upload_census_data/census_schema.py
@@ -0,0 +1,278 @@
+demo_schema = [
+                {
+                  'name': 'SUMLEV',
+                  'type': 'STRING'
+                },
+                {
+                  'name': 'STATE',
+                  'type': 'STRING'
+                },
+                {
+                  'name': 'COUNTY',
+                  'type': 'STRING'
+                },
+                {
+                  'name': 'TRACT',
+                  'type': 'STRING'
+                },
+                {
+                  'name': 'BLKGRP',
+                  'type': 'STRING'
+                },
+                {
+                  'name': 'BLOCK',
+                  'type': 'STRING'
+                },
+                {
+                  'name': 'POP100',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'HU100',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'INTPTLAT',
+                  'type': 'FLOAT'
+                },
+                {
+                  'name': 'INTPTLON',
+                  'type': 'FLOAT'
+                },
+                {
+                  'name': 'AREALAND',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'AREAWATER',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120001',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120002',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120003',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120004',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120005',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120006',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120007',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120008',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120009',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120010',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120011',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120012',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120013',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120014',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120015',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120016',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120017',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120018',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120019',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120020',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120021',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120022',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120023',
+                  'type': 'INTEGER'
+                },                
+                {
+                  'name': 'P0120024',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120025',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120026',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120027',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120028',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120029',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120030',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120031',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120032',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120033',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120034',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120035',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120036',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120037',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120038',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120039',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120040',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120041',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120042',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120043',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120044',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120045',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120046',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120047',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120048',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'P0120049',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'H0130001',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'H0130002',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'H0130003',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'H0130004',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'H0130005',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'H0130006',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'H0130007',
+                  'type': 'INTEGER'
+                },
+                {
+                  'name': 'H0130008',
+                  'type': 'INTEGER'
+                }
+              ]
---
+++ upload_census_data/googlebigqueryclient.py
@@ -0,0 +1,98 @@
+import logging
+import pprint
+import json
+import os
+
+import httplib2
+from apiclient.discovery import build
+from oauth2client.client import SignedJwtAssertionCredentials
+from googleapiclient.errors import HttpError
+
+from secret import GOOGLE_PRIVATE_KEY_FILE, GOOGLE_DEVELOPER_PROJECT_ID, GOOGLE_DEVELOPER_PROJECT_NUMBER, GOOGLE_SERVICE_EMAIL
+
+from census_schema import demo_schema 
+
+class GoogleBigQueryClient():
+    """
+    Google BigQuery API Client for GDELT access and processing.
+    """
+    def __init__(self):
+        """
+        Build authorized Google Big Query API Client from project_id and project_number given project google service email.
+        """
+        SCOPE = 'https://www.googleapis.com/auth/bigquery'
+        self.get_private_key()
+        credentials = SignedJwtAssertionCredentials(GOOGLE_SERVICE_EMAIL, self.private_key, SCOPE)
+        http = httplib2.Http()
+        self.http = credentials.authorize(http)
+        self.client = build('bigquery', 'v2', http=self.http)
+        
+    def get_private_key(self):
+        """
+        Read from *.pem private key file. File path specified in secrets.py.
+        """
+        BASE = os.path.dirname(os.path.abspath(__file__))
+        f = file(os.path.join(BASE, GOOGLE_PRIVATE_KEY_FILE), 'rb')
+        self.private_key = f.read()
+        f.close()
+
+    def test_access(self):
+        """
+        Small response test to test authorized client. Working as of 02/25/15.
+        """
+        datasets = self.client.datasets()
+        response = datasets.list(projectId=GOOGLE_DEVELOPER_PROJECT_NUMBER, all="true").execute()
+        print(response)
+        print('dataset list: \n')
+        for d in response['datasets']:
+            print("%s\n" % d['id'])
+
+def loadTable(service, projectId, datasetId, targetTableId, sourceCSV, tableSchema):
+  """
+  Load table from Google Cloud Storage into Big Query.
+  """
+  try:
+    jobCollection = service.jobs()
+    jobData = {
+      'projectId': projectId,
+      'configuration': {
+          'load': {
+            'sourceUris': [sourceCSV],
+            'skipLeadingRows': 1,
+            'schema': {
+              'fields': tableSchema
+            },
+            'destinationTable': {
+              'projectId': projectId,
+              'datasetId': datasetId,
+              'tableId': targetTableId
+            },
+          }
+        }
+      }
+
+    insertResponse = jobCollection.insert(projectId=projectId,
+                                         body=jobData).execute()
+    import time
+    while True:
+      job = jobCollection.get(projectId=projectId,
+                                 jobId=insertResponse['jobReference']['jobId']).execute()
+      if 'DONE' == job['status']['state']:
+          print 'Done Loading!'
+          return
+      print 'Waiting for loading to complete...'
+      time.sleep(10)
+
+    if 'errorResult' in job['status']:
+      print 'Error loading table: ', pprint.pprint(job)
+      return
+  except HttpError as err:
+    print 'Error in loadTable: ', pprint.pprint(err.resp)
+
+def main():
+  client = GoogleBigQueryClient()
+  # client.test_access()
+  # loadTable(client.client, GOOGLE_DEVELOPER_PROJECT_ID, "new_jersey", "new_jersey_demo", "gs://demographic-data/NewJerseyQuery.csv", demo_schema)
+
+if __name__ == "__main__":
+    main()
