002373c71c7934e1ca99d75327488cf12bd63e14
Hill Wyrough <alexander.hill.wyrough@gmail.com>
2015-03-19 14:18:25 -0400

    Adds command line arguments and interaction for running a state

--- make-the-country/make_the_state.py
+++ make-the-country/make_the_state.py
@@ -1,5 +1,8 @@
+import sys, argparse
+
 from population_client import get_all_tracts, get_demo_data, get_group_data, get_family_data, get_income_data
-from maker_helpers import confirm_command, confirm_output, display_start, display_stop
+from maker_helpers import confirm_execution, display_stop, display_start
+from maker_helpers import fix_state_name
 
 def make_the_state(state, county, output=True):
     """
@@ -10,9 +13,10 @@ def make_the_state(state, county, output=True):
     """
     # Validate Use
     confirm = confirm_execution(state, county, output)
-    if not confirm:
-        display_stop()
-        exit()
+    if not confirm: display_stop(); exit()
+    display_start(state, county, output)
+
+    internal_state = fix_state_name(state)
 
     # Get all the tracts for the state/county entity
 
@@ -30,8 +34,40 @@ def make_the_state(state, county, output=True):
         # get next tract
             # update income data
 
-# def main():
-#     make_the_state("new_jersey", None)
+def get_make_the_state_input():
+    print("[INPUT]: What state are you trying to make?")
+    state = raw_input("Type the name of your state (lower cased, underscored)...    ")
+    print("[INPUT]: Do you want to make the whole state? Or just a country?    ")
+    county = raw_input("Type the name of the county (or yes for the whole state)    ")
+    print("[INPUT]: Do you want to dump the results locally?    ")
+    output = raw_input("y/n to continue...    ")
 
-# if __name__=="__main__":
-#     main()
+    if (county == "yes"):
+        county = None
+    if output == "n":
+        output = False
+    else:
+        output = True
+    make_the_state(state, county, output)
+
+def main():
+    parser = argparse.ArgumentParser(description="Make the state simulations")
+    parser.add_argument('--interactive', metavar='I', dest="run", type=bool, default=False, help="a flag for interactive process")
+    parser.add_argument('--state', metavar="S", dest="state", type=str, help="the state")
+    parser.add_argument('--county', metavar="C", dest="county", type=str, help="the county")
+    parser.add_argument('--output', metavar="O", dest="output", type=bool, default=True, help="the output")
+
+    args = parser.parse_args()
+
+    if args.run:
+        if not args.state:
+            print("[ERROR]: you need to provide a valid state")
+            display_stop()
+        else:
+            make_the_state(args.state, args.county, args.output)
+    else:
+        get_make_the_state_input()
+
+
+if __name__=="__main__":
+    main()
--- make-the-country/maker_helpers.py
+++ make-the-country/maker_helpers.py
@@ -2,38 +2,105 @@
 User interaction and printing helpers for make_the_state.py
 """
 
-def confirm_output(state, county, output):
+def confirm_output(state, country, output):
     """
     Confirm where the user wants to put the output.
     """
-    pass
-
-def confirm_command(state, county):
-    """
-    Confirm command of state/county combination
-    """
-    if not county:
-        print("[WARNING]: you are attempting to recreate the entire state of %s. Is this what you want?", state)
-        go = raw_input("y/n to continue...")
+    message = "" \
+                "----------------------------------------------------\n" \
+                "* Please confirm where you want me to dump the      \n" \
+                "* results of this simulation.                       \n" \
+                "* This is very important!!!!!!!!!!!!!!!!!!!         \n" \
+                "* You don't want to over write data.                \n" \
+                "----------------------------------------------------\n"
+    print(message)
+    if not output:
+        print("[CONFIRM]: you are attempting to place the output of this simulation into GoogleBigQuery")
+        print("Is this what you want?")
+        go = raw_input("y/n to continue...    ")
     else:
-        print("[WARNING]: you are attempting to recreate the entire county of %s. Is this what you want? Press Enter.", county)
-        go = raw_input("y/n to continue...")
+        print("[CONFIRM]: you are attempting to place the output of this simulation into a local CSV file")
+        print("Is this what you want?")
+        go = raw_input("y/n to continue...    ")        
     return (go == "y")
 
-def confirm_execution(state, county, output):
+def confirm_command(state, country):
+    """
+    Confirm command of state/country combination
+    """
+    if not country:
+        print("[WARNING]: you are attempting to recreate the entire state of %s. Is this what you want?" % state)
+        go = raw_input("y/n to continue...    ")
+    else:
+        print("[WARNING]: you are attempting to recreate the entire country of %s. Is this what you want?" % country)
+        go = raw_input("y/n to continue...    ")
+    return (go == "y")
+
+def confirm_execution(state, country, output):
     """
     Return True if the execution should proceed as is, False if not.
     """
-    pass
+    command = confirm_command(state, country)
+    if not command:
+        return False
+    destination = confirm_output(state, country, output)
+    if not destination:
+        return False
+    return True
 
-def display_start(state, county, output):
+def display_start(state, country, output):
     """
     Display to user what he/she is attempting to do. Begin asking questions.
     """
-    pass
+    message = "" \
+                "----------------------------------------------------\n" \
+                "* BEGINNING...                                      \n" \
+                "* Strap in...here is what is about to happen...     \n" 
+
+    if not country:
+        message += "" \
+                "----------------------------------------------------\n" \
+                "* You are recreating the entire state of %s         \n" \
+                "----------------------------------------------------\n" % (state)
+    else:
+        message += "" \
+                "----------------------------------------------------\n" \
+                "* You are recreating the country of %s              \n" \
+                "* Which is located in the state of %s               \n" % (country, state)
+
+    if not output:
+        message += "" \
+                "----------------------------------------------------\n" \
+                "* The output of this simulation will be located in  \n" \
+                "* a GoogleBigQuery Table, I will tell you the name  \n" \
+                "* when we are finished.                             \n" \
+                "----------------------------------------------------\n"
+    else:
+        message += "" \
+                "----------------------------------------------------\n" \
+                "* The output of this simulation will be located in  \n" \
+                "* a local CSV file, I will tell you where it is when\n" \
+                "* we are finished.                                  \n" \
+                "----------------------------------------------------\n" 
+
+    message += "" \
+                "----------------------------------------------------\n" \
+                "* Sit back, and I will log progress reports         \n" \
+                "----------------------------------------------------\n"
+
+    print(message)
 
 def display_stop():
     """
     Display to user that you are stopping the execution.
     """
-    pass
+    message = "" \
+                "----------------------------------------------------\n" \
+                "* ABORTING...                                       \n" \
+                "* No damage done!                                   \n" \
+                "* Make the changes, and try again :)                \n" \
+                "----------------------------------------------------\n"
+    print(message)
+
+def fix_state_name(state):
+    return "_".join(state.lower().split(" "))
